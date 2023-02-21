434aa5ee44c0cec1611f1745e3b43b05d40cdd9d
marikafikys <marikaafikys@gmail.com>
2023-02-21 20:38:15 +0400

    feat: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
     constructor() {}
-
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.half = Math.round(this.min+(this.max - this.min)/2)
+        return this.half
     }
 
     lower() {
-
+        this.max = this.half
     }
 
     greater() {
-
+        this.min = this.half
     }
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame
